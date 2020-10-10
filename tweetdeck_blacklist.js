ae7cb37b3d07c5da7a05063a97474e5fe004efe4
argit2 <38925828+argit2@users.noreply.github.com>
2020-10-09 22:06:50 -0300

    fixed an issue about replies being inverted with user

--- tweetdeck_blacklist.js
+++ tweetdeck_blacklist.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Tweetdeck hide self retweets and blacklist retweets and replies of accounts
 // @namespace       https://github.com/argit2/tweetdeck-blacklist
-// @version         0.0.2
+// @version         0.0.3
 // @license         GPL-3.0-or-later
 // @supportURL      https://github.com/argit2/tweetdeck-blacklist
 // @description     Hide self retweets. Blacklist accounts to not see retweets of their posts. Very useful when you have a group of users that retweet each other.
