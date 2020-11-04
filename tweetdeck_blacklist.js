62b944c1fa63de888658ffa9647204b33f802b97
argit2 <38925828+argit2@users.noreply.github.com>
2020-11-03 21:33:27 -0300

    updated instructions

--- tweetdeck_blacklist.js
+++ tweetdeck_blacklist.js
@@ -12,14 +12,20 @@
 // ==/UserScript==
 
 /* INSTRUCTIONS
- * 1) Access tweetdeck.
- * 2) Alter the Storage tab of the script (it's above) and include the accounts you would like to not see retweets of.
- * 3) Click save.
- * You can check which tweets are being removed in the console in the developer tools of your browser.
- * In Firefox: Ctrl Shift K
- * In Chrome: Ctrl Shift J
- * PS: I use storage instead of a variable so your blacklisted accounts are preserved across updates of the script
- */
+
+Click on a user on tweetdeck to access the user profile. The button to add or remove from list will be there.
+
+You can see and modify the full list of users by doing this:
+
+1) Access tweetdeck.
+2) Click on tampermonkey icon, right click on the script to access the page where you edit the script. Go to the Storage tab of the script and include the accounts you would like to not see retweets of. The storage tab is only visible if you have the advanced settings turned on inside of Tampermonkey.
+3) Click save.
+You can check which tweets are being removed in the console in the developer tools of your browser.
+In Firefox: Ctrl Shift K
+In Chrome: Ctrl Shift J
+
+PS: I use storage instead of a variable so your blacklisted accounts are preserved across updates of the script
+*/
 
 // For self reference: based on my other youtube blacklist script, so any time you see "video" think it as "tweet".
 
